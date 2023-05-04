d8bc936f6830f0afe67a7ff20c8cb7543dd0823e
SairajBhoi <114217464+SairajBhoi@users.noreply.github.com>
2023-05-04 12:52:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "ap-south-1"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-02eb7a4783e7e9317" # us-west-2
   instance_type = "t2.micro"  
 }
